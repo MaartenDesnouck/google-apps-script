ce6263d32093cbdbc63a7c38d0d852e2d81db360
Maarten Desnouck <maarten.desnouck@gmail.com>
2017-06-04 14:07:58 +0200

    expand e2e test and fix bug found through e2e test

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-fail=0;
+succes=0;
 total=0;
 
 epoch=$(date +%s)
@@ -12,9 +12,13 @@ projectId1=''
 
 projectName2=$commonPart'_'$epoch'_project2'
 projectId2=''
+projectRootFolder2=$commonPart'_'$epoch'_project2_rootFolder'
 
 projectName3=$commonPart'_'$epoch'_project3'
 projectId3=''
+
+projectName4=$commonPart'_'$epoch'_project4'
+projectId4=''
 
 idLenght=57
 
@@ -28,11 +32,12 @@ echo '[Auth test]'
 
     total=$(($total+1))
     result=$(gas auth)
-    if [ "$result" != 'You are succesfully authenticated [✔]' ];
+    if [ "$result" = 'You are succesfully authenticated [✔]' ];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[1]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
 # Testing gas list and gas create
@@ -41,22 +46,25 @@ echo '\n[List, info and create test]'
     # Project we are going to create should not exist yet (1)
     total=$(($total+1))
     result=$(gas list $projectName1)
-    if [ "$result" != "No script projects matching the filter found in your Google Drive [✘]" ];
+    if [ "$result" = "No script projects matching the filter found in your Google Drive [✘]" ];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[2]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
+
 
     # Project we are going to create should not exist yet (2)
     total=$(($total+1))
     result=$(gas info $projectName1)
     pattern="No project with name or id '$projectName1' found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[3]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Create project 1
@@ -66,11 +74,12 @@ echo '\n[List, info and create test]'
     total=$(($total+1))
     result=$(gas list $projectName1)
     pattern="^\[(.{$idLenght})\] $projectName1$"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[4]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Parse projectId
@@ -80,32 +89,35 @@ echo '\n[List, info and create test]'
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="name:           $projectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[5]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Project we created should have info when we look it up using id
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="name:           $projectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[6]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Second project we are going to create should not exist yet
     result=$(gas list $projectName2)
     total=$(($total+1))
-    if [ "$result" != "No script projects matching the filter found in your Google Drive [✘]" ];
+    if [ "$result" = "No script projects matching the filter found in your Google Drive [✘]" ];
     then
-        echo "  fail[7]: gas list $projectName2"
+        succes=$(($succes+1))
+    else
+        echo "  fail[7]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Create project 2
@@ -115,11 +127,12 @@ echo '\n[List, info and create test]'
     total=$(($total+1))
     result=$(gas list $projectName2)
     pattern="^\[(.{$idLenght})\] $projectName2$"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[8]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Parse project id
@@ -129,36 +142,39 @@ echo '\n[List, info and create test]'
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="name:           $projectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[9]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # List with filter on common part should give us 2 results
     total=$(($total+1))
     result=$(gas list $commonPart)
     pattern="\[$projectId1\] $projectName1.*\[$projectId2\] $projectName2.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[10]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
 # Testing gas rename
 echo '\n[Rename test]'
 
-    # Project we are going to rename exists
+    # Project we are going to rename should exist
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="name:           $projectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[11]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Rename project
@@ -168,33 +184,36 @@ echo '\n[Rename test]'
     total=$(($total+1))
     result=$(gas info $newProjectName1)
     pattern="name:           $newProjectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[12]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Project with old name no longer exists
     total=$(($total+1))
     result=$(gas info $projectName1)
     pattern="No exact match found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[13]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Project with old id has a new name
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="name:           $newProjectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[14]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
 
@@ -205,11 +224,12 @@ echo '\n[Delete test]'
     total=$(($total+1))
     result=$(gas info $newProjectName1)
     pattern="name:           $newProjectName1.*id:             $projectId1.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[15]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Delete by name
@@ -219,22 +239,24 @@ echo '\n[Delete test]'
     total=$(($total+1))
     result=$(gas info $newProjectName1)
     pattern="No project with name or id '$newProjectName1' found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[16]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Project we deleted should no longer exists based on id
     total=$(($total+1))
     result=$(gas info $projectId1)
     pattern="No project with name or id '$projectId1' found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[17]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Create new project
@@ -244,11 +266,12 @@ echo '\n[Delete test]'
     total=$(($total+1))
     result=$(gas info $projectName3)
     pattern="name:           $projectName3.*id:             (.{$idLenght}).*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[18]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Parse projectId
@@ -258,11 +281,12 @@ echo '\n[Delete test]'
     total=$(($total+1))
     result=$(gas info $projectId3)
     pattern="name:           $projectName3.*id:             $projectId3.*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[19]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Delete project based on id
@@ -272,39 +296,225 @@ echo '\n[Delete test]'
     total=$(($total+1))
     result=$(gas info $projectName3)
     pattern="No project with name or id '$projectName3' found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[20]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
     # Project we deleted should no longer exist based on id
     total=$(($total+1))
     result=$(gas info $projectId3)
     pattern="No project with name or id '$projectId3' found in your Google Drive \[✘\].*"
-    if [[ ! "$result" =~ $pattern ]];
+    if [[ "$result" =~ $pattern ]];
     then
+        succes=$(($succes+1))
+    else
         echo "  fail[21]"
         echo "  $result"
-        fail=$(($fail+1))
     fi
 
-# TODO Testing gas pull
-# TODO Testing gas clone
-# TODO Testing gas new
-# TODO Testing gas push
-# TODO testing link
+# Testing gas link and pull
+echo '\n[Link and pull test]'
+
+    # Project we are going to link and pull should exists
+    total=$(($total+1))
+    result=$(gas info $projectId2)
+    pattern="name:           $projectName2.*id:             $projectId2.*"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[22]"
+        echo "  $result"
+    fi
+
+    #Linking and pulling using projectId
+    mkdir $projectRootFolder2
+    cd $projectRootFolder2
+    gas link $projectId2
+    gas pull
+    cd ..
+
+    # Main.js should exist in $projectRootFolder2
+    total=$(($total+1))
+    result=$(cat $projectRootFolder2/main.js)
+    pattern="function myFunction\(\) \{.*\}"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[23]"
+        echo "  $result"
+    fi
+
+    # ID should exist in $projectRootFolder2/.gas
+    total=$(($total+1))
+    result=$(cat $projectRootFolder2/.gas/ID)
+    if [ "$result" = $projectId2 ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[24]"
+        echo "  $result"
+    fi
+
+# Testing gas push and clone
+echo '\n[Push and clone test]'
+    # Create some files and push them
+    cd $projectRootFolder2
+    echo '//test' > test.js
+    gas push
+    cd ..
+
+    # Clone using projectId
+    gas clone $projectId2
+
+    # test.js should exist in $projectName2
+    total=$(($total+1))
+    result=$(cat $projectName2/test.js)
+    content='//test';
+    if [ "$result" = "$content" ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[25]"
+        echo "  $result"
+    fi
+
+    # main.js should exist in $projectName2
+    total=$(($total+1))
+    result=$(cat $projectName2/main.js)
+    pattern="function myFunction\(\) \{.*\}"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[26]"
+        echo "  $result"
+    fi
+
+    # ID should exist in $projectName2/.gas
+    total=$(($total+1))
+    result=$(cat $projectName2/.gas/ID)
+    if [ "$result" = $projectId2 ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[27]"
+        echo "  $result"
+    fi
+
+    # Delete folder
+    rm -r $projectName2
+
+    # main.js should not in $projectName2
+    total=$(($total+1))
+    if [ ! -f  $projectName2/main.js ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[28]"
+        echo "  $result"
+    fi
+
+    # Clone using projectName
+    gas clone $projectName2
+
+    # main.js should exist in $projectName2
+    total=$(($total+1))
+    result=$(cat $projectName2/main.js)
+    pattern="function myFunction\(\) \{.*\}"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[29]"
+        echo "  $result"
+    fi
+
+    # ID should exist in $projectName2/.gas
+    total=$(($total+1))
+    result=$(cat $projectName2/.gas/ID)
+    if [ "$result" = $projectId2 ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[30]"
+        echo "  $result"
+    fi
+
+
+# Testing gas new
+echo '\n[New test]'
+
+    # Project we are going to create should not exist yet
+    total=$(($total+1))
+    result=$(gas list $projectName4)
+    if [ "$result" = "No script projects matching the filter found in your Google Drive [✘]" ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[31]"
+        echo "  $result"
+    fi
+
+    gas new $projectName4
+
+    # Project we created shoud have info when we look it up using name
+    total=$(($total+1))
+    result=$(gas info $projectName4)
+    pattern="name:           $projectName4.*id:             (.{$idLenght}).*"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[5]"
+        echo "  $result"
+    fi
+
+    # Parse projectId
+    projectId4=${BASH_REMATCH[1]}
+
+    # main.js should exist in $projectName4
+    total=$(($total+1))
+    result=$(cat $projectName4/main.js)
+    pattern="function myFunction\(\) \{.*\}"
+    if [[ "$result" =~ $pattern ]];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[32]"
+        echo "  $result"
+    fi
+
+    # ID should exist in $projectName4/.gas
+    total=$(($total+1))
+    result=$(cat $projectName4/.gas/ID)
+    if [ "$result" = $projectId4 ];
+    then
+        succes=$(($succes+1))
+    else
+        echo "  fail[33]"
+        echo "  $result"
+    fi
+
 # TODO testing gas include
 
-# TODO Cleaning up at the end by deleting
-
+# Cleaning up at the end by deleting remaining projects and folders
+    gas delete $projectId2
+    gas delete $projectName4
+    rm -r $projectName2
+    rm -r $projectRootFolder2
+    rm -r $projectName4
 
 
 echo "-------------------------------------"
-echo "Test result: $(($total-$fail))/$total"
+echo "Test result: $(($succes))/$total"
 
-if [ $fail -gt 0 ];
+if [ $succes -gt 0 ];
 then
     exit 1
 else
